fbf0bfbd6423ac4f1fb54be2fbbb09f93102e254
torrent benoit <torrentdu64@gmail.com>
2016-12-07 09:03:05 +0100

    add has many to personnel model

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,3 +1,7 @@
 class Personnel < ActiveRecord::Base
   has_many :prestations
+  has_many :massages, through: :prestations
+  has_many :users, through: :prestations
+  has_many :accompagnements, through: :prestations
+
 end
