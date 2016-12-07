f0576da6081f5d5666d8925f7cf05f206c762a03
torrent benoit <torrentdu64@gmail.com>
2016-12-07 09:11:16 +0100

    add association user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :prestations
+  has_many :massages, through: :prestations
+  has_many :accompagnements, through: :prestations
+  has_many :personnels, through: :prestations
 end
