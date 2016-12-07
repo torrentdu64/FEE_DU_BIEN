463ea2614b88ff3e28199b73ef1d92ca44370fd7
torrent benoit <torrentdu64@gmail.com>
2016-12-07 14:14:29 +0100

    add  column  to Users nom prenom

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207111545) do
+ActiveRecord::Schema.define(version: 20161207131128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20161207111545) do
     t.datetime "updated_at",                          null: false
     t.string   "address"
     t.integer  "phone"
+    t.string   "nom"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
