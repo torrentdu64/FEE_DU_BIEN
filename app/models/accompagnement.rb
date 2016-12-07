352fd57661bba3986c51ae1d6b31a94154e6a8da
torrent benoit <torrentdu64@gmail.com>
2016-12-07 13:59:02 +0100

    validates accompagnement model

--- app/models/accompagnement.rb
+++ app/models/accompagnement.rb
@@ -3,4 +3,13 @@ class Accompagnement < ActiveRecord::Base
   has_many :personnels, through: :prestations
   has_many :users, through: :prestations
 
+  validates :nom, presence: true
+  validates :description, presence: true
+  validates :temps, numericality: true
+  validates :prix, numericality: true
+
 end
+
+
+
+
