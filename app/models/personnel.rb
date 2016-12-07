3c18ea68771f7433924b533acb677e50366da24d
torrent benoit <torrentdu64@gmail.com>
2016-12-07 14:05:27 +0100

    validation to personnel model

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -4,4 +4,13 @@ class Personnel < ActiveRecord::Base
   has_many :users, through: :prestations
   has_many :accompagnements, through: :prestations
 
+  validates :nom, presence: true
+  validates :prenom, presence: true
+  validates :description, presence: true
+  validates :adresse, presence: true
+  validates :horaire, presence: true
+  validates :phone, numericality: true, presence: true
+
 end
+
+
