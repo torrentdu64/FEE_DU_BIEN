4e6be99ba89978bf5d27c0c93e6bf32f20997f52
torrent benoit <torrentdu64@gmail.com>
2016-12-07 14:34:33 +0100

    validation prestation

--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -3,4 +3,8 @@ class Prestation < ActiveRecord::Base
   belongs_to :massage
   belongs_to :accompagnement
   belongs_to :personnel
+
+  validates :user, presence: true
+  validates :personnel, presence: true
+  validates :prestation, inclusion: { in: [:massage, :accompagnement] }
 end
