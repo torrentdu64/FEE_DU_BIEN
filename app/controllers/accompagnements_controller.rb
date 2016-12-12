85f9c13c72cba406a73ad8432fcc71501ce4be79
torrent benoit <torrentdu64@gmail.com>
2016-12-12 15:26:54 +0100

    controller accompagneent

--- app/controllers/accompagnements_controller.rb
+++ app/controllers/accompagnements_controller.rb
@@ -1,8 +1,17 @@
 class AccompagnementsController < ApplicationController
+
+  before_action :set_accompagnement, only: [:show, :crate]
+
   def new
+    @accompagnement = Accompagnement.new
   end
 
   def create
+     @accompagnement = Accompagnement.new(accompagnement_params)
+    if @dish.save
+      redirect_to user_profil_path
+    else
+      render :create
   end
 
   def update
@@ -18,5 +27,17 @@ class AccompagnementsController < ApplicationController
   end
 
   def index
+  end
+
+
+
+  private
+
+  def set_accompagnement
+    @accompagnement = Accompagnement.find(params[:id])
+  end
+
+  def accompagnement_params
+    params.require(:accompagnement).permit(:nom, :description, :temps, :prix)
   end
 end
--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -1,4 +1,5 @@
 class PrestationsController < ApplicationController
+
   def new
   end
 
